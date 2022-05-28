eec6d415fc26e05a1fa7c406c42314e694a2cd32
raymond-design <raymondyan777@gmail.com>
2022-05-28 12:17:30 -0700

    startup config functions

---
+++ config.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+type config struct {
+	id       string
+	httpPort string
+	raftPort string
+	pgPort   string
+}
+
+// get config details at startup
+func getConfig() config {
+	cfg := config{}
+	for i, arg := range os.Args[1:] {
+		if arg == "--node-id" {
+			cfg.id = os.Args[i+2]
+			i++
+			continue
+		}
+
+		if arg == "--http-port" {
+			cfg.httpPort = os.Args[i+2]
+			i++
+			continue
+		}
+
+		if arg == "--raft-port" {
+			cfg.raftPort = os.Args[i+2]
+			i++
+			continue
+		}
+
+		if arg == "--pg-port" {
+			cfg.pgPort = os.Args[i+2]
+			i++
+			continue
+		}
+	}
+
+	if cfg.id == "" {
+		log.Fatal("Missing required parameter: --node-id")
+	}
+
+	if cfg.raftPort == "" {
+		log.Fatal("Missing required parameter: --raft-port")
+	}
+
+	if cfg.httpPort == "" {
+		log.Fatal("Missing required parameter: --http-port")
+	}
+
+	if cfg.pgPort == "" {
+		log.Fatal("Missing required parameter: --pg-port")
+	}
+
+	return cfg
+}
